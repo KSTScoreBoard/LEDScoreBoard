a09f4a4559da6e299ef15c5abd70b6b5324f3728
KSTScoreBoard <kst.scoreboard@gmail.com>
2023-03-09 02:11:34 +0900

    リファクタリング

--- Software/seg7/seg7.cpp
+++ Software/seg7/seg7.cpp
@@ -5,6 +5,8 @@
 #include <stdio.h>
 #include "ST7032.h"
 
+#define DUTY_MAX 8
+
 #define addr_01 2
 #define addr_02 3
 
@@ -17,16 +19,7 @@
 
 #define _SCLR 4
 
-void shiftOut(uint8_t, uint8_t, uint8_t, uint32_t);
 void display();
-
-// application ID
-const uint32_t APP_ID = 0x1234abcd;
-// channel
-const uint8_t CHANNEL = 13;
-
-uint8_t addr = 0;
-
 ST7032 lcd;
 
 uint8_t segment[11] = {0b1111110,
@@ -40,56 +33,55 @@ uint8_t segment[11] = {0b1111110,
 					   0b1111111,
 					   0b1111011,
 					   0x00};
-uint8_t score[3] = {0,0,0};
-uint16_t bright = 128;
+int16_t score = 0;
+int16_t brightness = 8;
 uint8_t lq = 0;
 
 /*** setup procedure (run once at cold boot) */
 void setup() {
+	//setup pinMode
+	{
+		pinMode(addr_01,INPUT_PULLUP);
+		pinMode(addr_02,INPUT_PULLUP);
+		pinMode(Button_PlusOne,INPUT_PULLUP);
+		pinMode(Button_MinusOne,INPUT_PULLUP);
+		pinMode(Button_PlusTen,INPUT_PULLUP);
+		pinMode(Button_MinusTen,INPUT_PULLUP);
+		pinMode(Button_PlusHun,INPUT_PULLUP);
+		pinMode(Button_MinusHun,INPUT_PULLUP);
+		pinMode(_SCLR,OUTPUT);
+
+		Buttons.setup(5);
+		Buttons.begin(1UL << 5, 5, 10);
+		Buttons.setup(5);
+		Buttons.begin(1UL << Button_PlusOne | 1UL << Button_MinusOne | 
+					1UL << Button_PlusTen | 1UL << Button_MinusTen |
+					1UL << Button_PlusHun | 1UL << Button_MinusHun,5,10);
+	}
+
 	// the twelite main class
 	auto&& brd = the_twelite.board.use<BRD_APPTWELITE>();
 	the_twelite
-		<< TWENET::appid(APP_ID)    // set application ID (identify network group)
-		<< TWENET::channel(CHANNEL) // set channel (pysical channel)
+		<< TWENET::appid(0x1234abcd)    // set application ID (identify network group)
+		<< TWENET::channel(13) // set channel (pysical channel)
 		<< TWENET::rx_when_idle();  // open receive circuit (if not set, it can't listen packts from others)
 	
 	// Register Network
+	uint8_t addr = (digitalRead(addr_01)  ==  HIGH ? 0 : 1) | (digitalRead(addr_02) == HIGH ? 0 : 1) << 1;
 	auto&& nwksmpl = the_twelite.network.use<NWK_SIMPLE>();
-	nwksmpl << NWK_SIMPLE::logical_id(0xFE) // set Logical ID. (0xFE means a child device with no ID)
+	nwksmpl << NWK_SIMPLE::logical_id(addr) // set Logical ID. (0xFE means a child device with no ID)
 	        << NWK_SIMPLE::repeat_max(3);   // can repeat a packet up to three times. (being kind of a router)
 	
 	the_twelite.begin(); // start twelite!
 	Timer1.end();
 	Timer2.end();
-	SPI.begin(0, SPISettings(100000, SPI_CONF::LSBFIRST, SPI_CONF::SPI_MODE0));
-
-	Buttons.setup(5);
-    Buttons.begin(1UL << 5, 5, 10);
-	
 	Timer4.change_hz(10000);
-	Timer4.change_duty(256 - bright,256);
-
+	Timer4.change_duty(DUTY_MAX - brightness,DUTY_MAX);
+	SPI.begin(0, SPISettings(100000, SPI_CONF::LSBFIRST, SPI_CONF::SPI_MODE0));
+	
 	lcd.begin(8,2);
     lcd.setContrast(25);
 
-	pinMode(addr_01,INPUT_PULLUP);
-	pinMode(addr_02,INPUT_PULLUP);
-	pinMode(Button_PlusOne,INPUT_PULLUP);
-	pinMode(Button_MinusOne,INPUT_PULLUP);
-	pinMode(Button_PlusTen,INPUT_PULLUP);
-	pinMode(Button_MinusTen,INPUT_PULLUP);
-	pinMode(Button_PlusHun,INPUT_PULLUP);
-	pinMode(Button_MinusHun,INPUT_PULLUP);
-	pinMode(_SCLR,OUTPUT);
-
-	addr = (digitalRead(addr_01)  ==  HIGH ? 0 : 1) | (digitalRead(addr_02) == HIGH ? 0 : 1) << 1;
-	Serial << format("%d\n",addr) << crlf;
-
-	Buttons.setup(5);
-	Buttons.begin(1UL << Button_PlusOne | 1UL << Button_MinusOne | 
-				  1UL << Button_PlusTen | 1UL << Button_MinusTen |
-				  1UL << Button_PlusHun | 1UL << Button_MinusHun,5,10);
-	
 	char str[64];
 	sprintf(str,"id=%d\n",addr);
 	lcd.print(str);
@@ -103,32 +95,35 @@ void loop() {
         uint32_t bm, cm;
         Buttons.read(bm, cm);
 
+		int _score[3] = {score / 100, (score%100)/10, score % 10};
 		if(!(bm & (1UL << Button_MinusHun | 1UL << Button_PlusOne))){
-			bright += 128;		
+			brightness += 1;
 		}else if(!(bm & (1UL << Button_MinusHun | 1UL << Button_MinusOne))){
-			bright -= 128;
+			brightness -= 1;
 		}else if (!(bm & (1UL << Button_PlusOne))) {
-			score[2]++;
+			_score[2]++;
         }else if(!(bm & (1UL << Button_MinusOne))){
-            score[2]--;
+            _score[2]--;
         }else if(!(bm & (1UL << Button_PlusTen))){
-            score[1]++;
+            _score[1]++;
         }else if(!(bm & (1UL << Button_MinusTen))){
-            score[1]--;
+            _score[1]--;
         }else if(!(bm & (1UL << Button_PlusHun))){
-            score[0]++;
+            _score[0]++;
         }else if(!(bm & (1UL << Button_MinusHun))){
-            score[0]--;
+            _score[0]--;
         }
 
+		if(brightness >= 8) brightness = 8;
+		if(brightness < 0) brightness = 0;
+		if(_score[0] < 0) _score[0] = 9;
+		if(_score[1] < 0) _score[1] = 9;	
+		if(_score[2] < 0) _score[2] = 9;
+		if(_score[0] > 9) _score[0] = 0;
+		if(_score[1] > 9) _score[1] = 0;
+		if(_score[2] > 9) _score[2] = 0;
 		
-		if(score[0] >= 255) score[0] = 9;
-		if(score[1] >= 255) score[1] = 9;	
-		if(score[2] >= 255) score[2] = 9;
-		if(score[0] > 9) score[0] = 0;
-		if(score[1] > 9) score[1] = 0;
-		if(score[2] > 9) score[2] = 0;
-
+		score = _score[0]*100 + _score[1]*10 + _score[2];
 		display();
     }
 }
@@ -136,12 +131,12 @@ void loop() {
 void on_rx_packet(packet_rx& rx, bool_t &handled) {
 	// rx >> Serial; // debugging (display longer packet information)
 	uint16_t _score = 0;
-	uint16_t _bright = 0;
+	uint16_t _brightness = 0;
 
 	// expand packet payload (shall match with sent packet data structure, see pack_bytes())
 	expand_bytes(rx.get_payload().begin(), rx.get_payload().end()
 				, _score
-				, _bright
+				, _brightness
 	);
 
 	// display the packet
@@ -151,31 +146,31 @@ void on_rx_packet(packet_rx& rx, bool_t &handled) {
 				, rx.get_length()
 				, rx.get_psRxDataApp()->u8Seq
 				)
-			<< format(" score=%d, bright=%d>" // note: up to 4 args!
+			<< format(" score=%d, brightness=%d>" // note: up to 4 args!
 				, _score
-				, _bright
+				, _brightness
 				)
 			<< mwx::crlf
 			<< mwx::flush;
 
-	score[0] = _score / 100;
-	score[1] = (_score%100)/10;
-	score[2] = _score % 10;
-	bright = _bright;
+	score = int16_t(_score);
+	brightness = int16_t(_brightness);
 	lq = rx.get_lqi();
 	display();
 }
 
 void display(){
+	int _score[3] = {score / 100, (score%100)/10, score % 10};
 	if (auto&& trs = SPI.get_rwer()) { // オブジェクトの生成とデバイスの通信判定
 		// このスコープ(波かっこ)内が trs の有効期間。
-		trs << (uint8_t)(segment[score[0]]);
-		trs << (uint8_t)(segment[score[1]]);
-		trs << (uint8_t)(segment[score[2]]);
+		trs << (uint8_t)(segment[_score[0]]);
+		trs << (uint8_t)(segment[_score[1]]);
+		trs << (uint8_t)(segment[_score[2]]);
 	}
-	Timer4.change_duty(256 - bright,256);
+	if(brightness >=  8) brightness = 8;  
+	Timer4.change_duty(DUTY_MAX - brightness ,DUTY_MAX);
 
 	char str[64];
-	sprintf(str,"%d%d%d %d\nq=%d%",score[0],score[1],score[2],bright,(lq*100/255));
+	sprintf(str,"%03d %d\nq=%d%",score,brightness,(lq*100/255));
 	lcd.print(str);
 }
